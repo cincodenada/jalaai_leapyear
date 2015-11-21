e0df251695e6f7f90155025fc43369f38a5c5266
Joel Bradshaw <cincodenada@gmail.com>
2015-11-21 00:26:57 -0800

    Adjust for Tehran time, tweak styles

--- jalaali.R
+++ jalaali.R
@@ -1,6 +1,8 @@
 library(ggplot2);
+library(grid);
 diff = read.csv('leap_diff.tsv', sep="\t");
-diff$hrs = diff$diff*24
+# Convert to hours and adjust for Tehran time (UTC + 3.425 hrs)
+diff$hrs = diff$diff*24 + 3.425
 cycles = read.csv('leap_cycles.tsv', sep="\t");
 # Shift the cycles back two years, center brackets
 # On the middle of the 5-year cycle
@@ -9,25 +11,27 @@ cycles$end = cycles$end - 2;
 # Calculate mids and lengths
 cycles$mid = (cycles$start + cycles$end)/2
 cycles$len = (cycles$end - cycles$start)
-ypos = -0.75*24
-ticksize = 0.025*24
+ypos = -14
+ticksize = 0.5
 
 linecol = "blue";
 scalecol = "gray30";
 
 png('offset.png',w=1000,h=500);
-ggplot() + 
+ggplot() +
     geom_line(data=diff,aes(year,hrs),color=linecol) + geom_point(data=diff,aes(year,hrs),color=linecol,shape=19) +
     geom_segment(data=cycles, aes(x=start,xend=end,y=ypos,yend=ypos), color=scalecol) +
-    geom_segment(data=cycles, aes(x=start,xend=start,y=ypos+ticksize,yend=ypos-ticksize), color=scalecol) +
-    geom_segment(data=cycles, aes(x=end,xend=end,y=ypos+ticksize,yend=ypos-ticksize), color=scalecol) +
+    geom_segment(data=cycles, aes(x=start,xend=start,y=ypos+ticksize,yend=ypos), color=scalecol) +
+    geom_segment(data=cycles, aes(x=end,xend=end,y=ypos+ticksize,yend=ypos), color=scalecol) +
     geom_text(data=cycles, aes(label=len, x=mid), y=ypos-ticksize*1.5, color=scalecol) +
     scale_y_continuous(expand=c(0,5)) +
-    scale_x_continuous(expand=c(0,0), labels = function(y) { paste(y,"\n(",y+621,")",sep="") }) +
+    scale_x_continuous(expand=c(0,0), labels = function(y) { paste(y,"\n(",y+621,")\n",sep="") }) +
     theme_bw() +
-    labs(title="Jalaai Leap Years", x="Jalaai Year\n(Gregorian Year)", y="Diff Between Solstice and New Year (hours)") +
+    labs(title=expression(bold("Jalaai Leap Years\n")), x="Jalaai Year\n(Gregorian Year)", y="Diff Between Solstice and New Year (hours)\n") +
     theme(
+        aspect.ratio = 2/(1+sqrt(5)),
         panel.grid.major = element_line(color="gray50"),
-        panel.border = element_rect(color="black")
+        panel.border = element_rect(color="black"),
+        plot.margin = unit(c(24,12,12,12), "points")
     );
 dev.off();
