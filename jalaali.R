bc1295e3ca85cfc881d444dfcb383758a0514298
Joel Bradshaw <cincodenada@gmail.com>
2015-11-21 00:34:20 -0800

    Tweak SVG output

--- jalaali.R
+++ jalaali.R
@@ -17,19 +17,20 @@ ticksize = 0.5
 linecol = "blue";
 scalecol = "gray30";
 
-png('offset.png',w=1000,h=500);
+#png('offset.png',w=1000,h=500);
+svg('offset.svg');
 ggplot() +
-    geom_line(data=diff,aes(year,hrs),color=linecol) + geom_point(data=diff,aes(year,hrs),color=linecol,shape=19) +
+    geom_line(data=diff,aes(year,hrs),color=linecol,size=0.25) + geom_point(data=diff,aes(year,hrs),color=linecol,shape=19, size=0.9) +
     geom_segment(data=cycles, aes(x=start,xend=end,y=ypos,yend=ypos), color=scalecol) +
     geom_segment(data=cycles, aes(x=start,xend=start,y=ypos+ticksize,yend=ypos), color=scalecol) +
     geom_segment(data=cycles, aes(x=end,xend=end,y=ypos+ticksize,yend=ypos), color=scalecol) +
-    geom_text(data=cycles, aes(label=len, x=mid), y=ypos-ticksize*1.5, color=scalecol) +
+    geom_text(data=cycles, aes(label=len, x=mid), y=ypos-ticksize*1.5, color=scalecol, size=3) +
     scale_y_continuous(expand=c(0,5)) +
     scale_x_continuous(expand=c(0,0), labels = function(y) { paste(y,"\n(",y+621,")\n",sep="") }) +
     theme_bw() +
     labs(title=expression(bold("Jalaai Leap Years\n")), x="Jalaai Year\n(Gregorian Year)", y="Diff Between Solstice and New Year (hours)\n") +
     theme(
-        aspect.ratio = 2/(1+sqrt(5)),
+        aspect.ratio = 1/2.5,
         panel.grid.major = element_line(color="gray50"),
         panel.border = element_rect(color="black"),
         plot.margin = unit(c(24,12,12,12), "points")
